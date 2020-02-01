3c73e753f6972bae44bb06fad14930d5259c3685
John Nielsen <archangel@lestatdcourtsmb.lan>
2020-01-31 21:01:10 -0700

    Add BurgerBuilder Container

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Aux from '../../hoc/Aux';
+
+class BurgerBuilder extends Component {
+    render() {
+        return (
+            <Aux>
+                <div>TODO: Burger Component</div>
+                <div>TODO: Build Control Component</div>
+            </Aux>
+        )
+    }
+}
+
+export default BurgerBuilder;
