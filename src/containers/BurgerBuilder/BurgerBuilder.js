2f7e1e95bf8e19cafda8a1264c7902c7a9f91c01
John Nielsen <archangel@lestatdcourtsmb.lan>
2020-02-02 12:24:40 -0700

    Add Build Control Component and CSS Modules

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
+
 import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger'
+import BuildControls from '../../components/Burger/BuildControls/BuildControls'
 
 class BurgerBuilder extends Component {
     state = {
         ingredients: {
-            'bread-top'     : 1,
-            'salad'         : 1,
-            'bacon'         : 1,
-            'cheese'        : 2,
-            'meat'          : 2,
-            'bread-bottom'  : 1
+            'salad'         : 0,
+            'bacon'         : 0,
+            'cheese'        : 0,
+            'meat'          : 0,
         }
     };
 
@@ -18,7 +18,7 @@ class BurgerBuilder extends Component {
         return (
             <Aux>
                 <Burger ingredients={this.state.ingredients}/>
-                <div>TODO: Build Control Component</div>
+                <BuildControls/>
             </Aux>
         )
     }
