4f837ad7bea5bbdea501b26911dffe88052204c4
John Nielsen <archangel@Lestat-De-Lioncourts-MB.local>
2020-02-04 21:18:25 -0700

    Add state for adding ingredients and removing ingredients

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -4,18 +4,24 @@ import classes from './BuildControls.module.css'
 import BuildControl from './BuildControl/BuildControl'
 
 const controls = [
-    { label: 'Salad'        , type: 'salad'},
-    { label: 'Bacon'        , type: 'bacon'},
-    { label: 'Cheese'       , type: 'cheese'},
-    { label: 'Meat'         , type: 'meat'},
+    { label: 'Salad'    , type: 'salad'},
+    { label: 'Bacon'    , type: 'bacon'},
+    { label: 'Cheese'   , type: 'cheese'},
+    { label: 'Meat'     , type: 'meat'},
 ];
 
 
 const buildControls = (props) => (
     <div className={classes.BuildControls}>
         {controls.map(ctrl => (
-            <BuildControl key={ctrl.label} label={ctrl.label}/>
+            <BuildControl
+                key={ctrl.label}
+                label={ctrl.label}
+                added={() => props.ingredientAdded(ctrl.type)}
+                removed={() => props.ingredientSubracted(ctrl.type)}
+            />
         ))}
+        <label>Price: {props.totalPrice}</label>
     </div>
 );
 
